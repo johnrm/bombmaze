449688d23227b5dc831707b5e36290a89f5f52ec
johnrm <notjohnrm@gmail.com>
2024-03-31 17:43:38 +0000

    Add grid build and display for testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,30 @@
-function buildGrid (width, height) {
-    let grid=[];
-    let row=[];
-    for (let y=0; y<10; y++) {
-        row=[];
-        for (let x=0; x<10; x++) {
-            row.push(Math.floor(Math.random()*2))
+
+let grid=[];
+buildGrid(10, 10);
+displayGrid();
+
+/**
+ * Build Grid function
+ * This generates the grid and populates it with game objects.
+ */
+function buildGrid(height, width) {
+    let rowData = [];
+    console.log(height, 'x', width);
+    for (let row = 0; row < height; row++) {
+        rowData = [];
+        for (let col = 0; col < width; col++) {
+            rowData.push(Math.floor(Math.random() * 10))
         }
+        grid.push(rowData);
     }
+}
+
+/**
+ * Display Grid
+ * This dumps the grid to index.html.
+ */
+function displayGrid() {
+    for (let row in grid) {
+        document.write(grid[row] + '<br>');
     }
+}
